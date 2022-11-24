43eaed6f4651835ab74a2bcdbbb878bafd0646fe
Elvis <elviscruz45@gmail.com>
2022-11-24 07:35:21 -0500

    class4

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 **¡Sígueme en Twitch para streams sobre desarrollo web!** 👉 https://www.twitch.tv/midudev
 
 Repositorio con el código del [Curso Avanzado de React de Platzi](https://platzi.com/cursos/react-avanzado/)
+
+
+
+1. INICIO CURSO
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app")
+const root = createRoot(container)
+root.render(<h1>Segyuuimos con el curso avanzado</h1>)
+
+
+//import React from "react"
+//import {createRoot} from "react-dom/client"
+
+//const container=document.getElementById("app")
+//const root=creatRoot(container)
+//root.render("con este render , va a renderizar algo")
